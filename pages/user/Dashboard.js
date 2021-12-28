4e3b2e740a01c35783ca4536125e40c204ea5c1c
walterjrr <jrwalter731@gmail.com>
2021-12-28 15:00:51 -0300

    adcionando template Default, Header e dashboard

--- pages/user/Dashboard.js
+++ pages/user/Dashboard.js
@@ -3,10 +3,19 @@ import Container from '@material-ui/core/Container'
 import { Typography } from "@material-ui/core"
 import { makeStyles } from '@material-ui/core/styles'
 import { Button } from "@material-ui/core"
+import { Grid } from "@material-ui/core"
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles(() => ({
   Container: {
     padding: '20px'
+  },
+  cardMedia: {
+    paddingTop: '56%',
   },
   buttonAdd: {
     margin: '30px auto',
@@ -26,7 +35,95 @@ function Home() {
           <Button variant="contained" color="primary" className={classes.buttonAdd}>
             Publicar Novo Anuncio
           </Button>
-        </Container>  
+        </Container>
+        <Container maxWidth="md">
+          <Grid Container spacing={4} >
+            <Grid item xs={12} sm={6} md={3}>
+              <Card>
+                <CardMedia
+                  className={classes.cardMedia}
+                  title="titulo da imagem"
+                  image="https://source.unsplash.com/random"
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    Produto X
+                  </Typography>
+                  <Typography >
+                    R$ 98,00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">Editar</Button>
+                  <Button size="small" color="primary">Remover</Button>
+                </CardActions>
+              </Card>
+            </Grid> 
+            <Grid item xs={12} sm={6} md={3}>
+              <Card>
+                <CardMedia
+                  className={classes.cardMedia}
+                  title="titulo da imagem"
+                  image="https://source.unsplash.com/random"
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    Produto X
+                  </Typography>
+                  <Typography >
+                    R$ 98,00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">Editar</Button>
+                  <Button size="small" color="primary">Remover</Button>
+                </CardActions>
+              </Card>
+            </Grid> 
+            <Grid item xs={12} sm={6} md={3}>
+              <Card>
+                <CardMedia
+                  className={classes.cardMedia}
+                  title="titulo da imagem"
+                  image="https://source.unsplash.com/random"
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    Produto X
+                  </Typography>
+                  <Typography >
+                    R$ 98,00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">Editar</Button>
+                  <Button size="small" color="primary">Remover</Button>
+                </CardActions>
+              </Card>
+            </Grid> 
+            <Grid item xs={12} sm={6} md={3}>
+              <Card>
+                <CardMedia
+                  className={classes.cardMedia}
+                  title="titulo da imagem"
+                  image="https://source.unsplash.com/random"
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    Produto X
+                  </Typography>
+                  <Typography >
+                    R$ 98,00
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">Editar</Button>
+                  <Button size="small" color="primary">Remover</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid> 
+        </Container> 
       </TemplateDefault>
     </>
   )
